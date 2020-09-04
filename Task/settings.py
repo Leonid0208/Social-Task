c729088e2536672c0c15896ee187b0f2c403db70
Leonid0208 <themusik.batbka@gmail.com>
2020-09-04 19:20:48 +0300

    Added files and changes required for deployment to heroku

--- Task/settings.py
+++ Task/settings.py
@@ -134,6 +134,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 import dj_database_url
+import django_heroku
+django_heroku.settings(locals())
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
